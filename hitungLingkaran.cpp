39a95d87f7ac8dbb2dc558ad15753e9de8e8018c
Wildan Scott Kennedy <afandikaw@gmail.com>
2026-04-23 18:33:00 +0800

    pre

---
+++ hitungLingkaran.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    double bilanganSatu = 14;
+    double jariJari = 3.14;
+    double totalKombinasi = 2 * jariJari * bilanganSatu;
+    double totalLuas = 2 * bilanganSatu * bilanganSatu;
+
+
+    cout << "Total lingkaran: " << totalKombinasi << endl;
+    cout << "Total luas: " << totalLuas << endl;
+    
+
+    return 0;
+}
