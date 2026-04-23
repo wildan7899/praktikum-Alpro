48e89f1a2a3bb39c3028b49a4ca29678b36c1620
Wildan Scott Kennedy <afandikaw@gmail.com>
2026-04-23 17:52:54 +0800

    pre

--- preAssist.cpp
+++ preAssist.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main () {
 
-    int hargaBeras = 10000, jumlahBeras = 3, jumlahMinyak = 3, hargaMinyak = 10000, jumlahGula = 3, hargaGula = 10000;
-    int uangBayar = 100000;
+    int hargaBeras = 10000, jumlahBeras, jumlahMinyak, hargaMinyak = 10000, jumlahGula, hargaGula = 10000;
+    int uangPengguna;
     int diskon = 4;
 
     int totalBeras = hargaBeras * jumlahBeras;
@@ -13,19 +13,27 @@ int main () {
     int totalBelanja = totalBeras + totalMinyak + totalGula;
     int diskonPengguna = totalBelanja / diskon;
     int totalHarusDibayar = totalBelanja - diskonPengguna;
-    int uangKembalian = uangBayar - totalHarusDibayar;
+    int uangKembalian = uangPengguna - totalHarusDibayar;
 
+    cout << "Masukkan uang yang anda miliki: ";
+    cin >> uangPengguna;
+    cout << "Masukkan jumlah beras yang dibeli: ";
+    cin >> jumlahBeras;
+    cout << "Masukkan jumlah minyak yang dibeli: ";
+    cin >> jumlahMinyak;
+    cout << "Masukkan jumlah gula yang dibeli: ";
+    cin >> jumlahGula;
 
 
     cout << "=============================" << endl;
-    cout << "Total beras: Rp " << totalBeras << endl;
-    cout << "Total minyak: Rp " << totalMinyak << endl;
-    cout << "Total gula: Rp " << totalGula << endl;
+    cout << "Total beras: Rp" << totalBeras << endl;
+    cout << "Total minyak: Rp" << totalMinyak << endl;
+    cout << "Total gula: Rp" << totalGula << endl;
     cout << "=============================" << endl;
-    cout << "Total belanja: Rp " << totalBelanja << endl;
-    cout << "Diskon: Rp " << diskonPengguna << endl;
-    cout << "Total yang harus dibayar: Rp " << totalHarusDibayar << endl;
-    cout << "Uang kembalian: Rp " << uangKembalian << endl;
+    cout << "Total belanja: Rp" << totalBelanja << endl;
+    cout << "Diskon: Rp" << diskonPengguna << endl;
+    cout << "Total yang harus dibayar: Rp" << totalHarusDibayar << endl;
+    cout << "Uang kembalian: Rp" << uangKembalian << endl;
     cout << "=============================" << endl;
 
 
