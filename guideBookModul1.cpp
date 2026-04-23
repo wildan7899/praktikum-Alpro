bf0f851cf3f9624d4c3b2e1acd1108a67803e662
Wildan Scott Kennedy <afandikaw@gmail.com>
2026-04-23 17:37:28 +0800

    preAssist

--- guideBookModul1.cpp
+++ guideBookModul1.cpp
@@ -3,6 +3,21 @@ using namespace std;
 
 int main () {
 
+    int hargaTiket = 30000, jumlahTiket = 2, uangBayar = 100000, diskonPersen = 10, jumlahOrang = 2;
+
+    int totalHarga = hargaTiket * jumlahTiket;
+    int diskon = totalHarga * diskonPersen;
+    int totalSetelahDiskon = totalHarga - diskon;
+    int kembalian = uangBayar - totalSetelahDiskon;
+    int perOrang = kembalian / jumlahOrang;
+
+
+    cout << "Total harga: Rp " << totalHarga << endl;
+    cout << "Diskon: Rp " << diskon << endl;
+    cout << "Setelah diskon: Rp " << totalSetelahDiskon << endl;
+    cout << "Kembalian : Rp " << kembalian << endl;
+    cout << "Per orang dapat: Rp " << perOrang << endl;
+
   
 
 
--- preAssist.cpp
+++ preAssist.cpp
@@ -3,6 +3,30 @@ using namespace std;
 
 int main () {
 
+    int hargaBeras = 10000, jumlahBeras = 3, jumlahMinyak = 3, hargaMinyak = 10000, jumlahGula = 3, hargaGula = 10000;
+    int uangBayar = 100000;
+    int diskon = 4;
+
+    int totalBeras = hargaBeras * jumlahBeras;
+    int totalMinyak = hargaMinyak * jumlahMinyak;
+    int totalGula = hargaGula * jumlahGula;
+    int totalBelanja = totalBeras + totalMinyak + totalGula;
+    int diskonPengguna = totalBelanja / diskon;
+    int totalHarusDibayar = totalBelanja - diskonPengguna;
+    int uangKembalian = uangBayar - totalHarusDibayar;
+
+
+
+    cout << "=============================" << endl;
+    cout << "Total beras: Rp " << totalBeras << endl;
+    cout << "Total minyak: Rp " << totalMinyak << endl;
+    cout << "Total gula: Rp " << totalGula << endl;
+    cout << "=============================" << endl;
+    cout << "Total belanja: Rp " << totalBelanja << endl;
+    cout << "Diskon: Rp " << diskonPengguna << endl;
+    cout << "Total yang harus dibayar: Rp " << totalHarusDibayar << endl;
+    cout << "Uang kembalian: Rp " << uangKembalian << endl;
+    cout << "=============================" << endl;
 
 
     return 0;
